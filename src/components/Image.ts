2e8ab3ce8e87273324974262ee51ad1b7db69bea
Yanita Biheza <bihezayanita@gmail.com>
2024-11-23 18:53:24 +0300

    fix: add previous solution back

--- src/components/Image.ts
+++ src/components/Image.ts
@@ -1,12 +1,12 @@
 import ImageType from '../types';
   
   export default function createImage(image: ImageType): string {
-    const imgUrl = new URL(`../../${image.name}`, import.meta.url).href
     return `
       <img 
-        src="${imgUrl}" 
+        src="src/assets/images/${image.name}" 
         alt="${image.alt}" 
         class="card-image" 
       />
     `;
   }
+  
